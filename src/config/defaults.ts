00f9d80bec1c2dc9b42678b5477160ec7da214c9
Jaki Chen <JakiChen@live.cn>
2024-12-08 10:27:52 +0800

    Update defaults.ts

--- src/config/defaults.ts
+++ src/config/defaults.ts
@@ -12,7 +12,7 @@ export const defaults: Options = {
         purpose: "maskable",
         transparent: true,
         rotate: false,
-        offset: 0,
+        offset: 13,
       }
     ],
     appleIcon: [
