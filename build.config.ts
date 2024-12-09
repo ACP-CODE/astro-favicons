14915f316b38a3e91b37b0bf0cb9fa14da210093
Jaki Chen <JakiChen@live.cn>
2024-12-10 07:56:04 +0800

    Update build.config.ts

--- build.config.ts
+++ build.config.ts
@@ -4,7 +4,6 @@ export default defineBuildConfig({
   entries: ["./src/index", "./src/middleware"],
   externals: ["virtual:astro-favicons"],
   declaration: true,
-  failOnWarn: false,
   rollup: {
     emitCJS: false,
   },
