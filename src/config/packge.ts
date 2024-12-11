eff692814e8e12a06c6718529771303b307b0aae
Jaki Chen <JakiChen@live.cn>
2024-12-11 22:13:27 +0800

    Update packge.ts

--- src/config/packge.ts
+++ src/config/packge.ts
@@ -1,2 +1,5 @@
 import { name, version, homepage } from "../../package.json";
-export { name, version, homepage };
+const formatedName =
+  name.replace(/-/g, " ").replace(/\b\w/g, (l) => l.toUpperCase()) +
+  ` integration`;
+export { name, formatedName, version, homepage };
